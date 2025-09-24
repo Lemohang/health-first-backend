d9523e99ad41c05ae2dae224f8d745418745dc7c
Lemohang57 <lemohangmakintane57@gmail.com>
2025-09-24 21:43:09 +0200

    endpoints

--- app/back/health_platform/appointments/urls.py
+++ app/back/health_platform/appointments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AppointmentListCreateView, AvailabilityListCreateView
 
 urlpatterns = [
-    path('appointments/', AppointmentListCreateView.as_view(), name='appointments'),
+    path('', AppointmentListCreateView.as_view(), name='appointments'),
     path('availability/', AvailabilityListCreateView.as_view(), name='availability'),
 ]
--- app/back/health_platform/users/admin.py
+++ app/back/health_platform/users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Profile
 
-# Register your models here.
+# Customize User admin
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Role & Extra Info", {"fields": ("is_patient", "is_provider", "location")}),
+    )
+    list_display = ("username", "email", "is_patient", "is_provider", "location", "is_staff")
+    list_filter = ("is_patient", "is_provider", "is_staff")
+    search_fields = ("username", "email", "location")
+
+# Profile inline for quick edit from User page
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+# Register models
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
