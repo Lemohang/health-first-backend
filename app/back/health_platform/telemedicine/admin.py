3043a6850e6bb4e25a45d70dad0c8c6b58321481
Lemohang57 <lemohangmakintane57@gmail.com>
2025-09-25 21:51:56 +0200

    tele

--- app/back/health_platform/telemedicine/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import VideoSession
-
-@admin.register(VideoSession)
-class VideoSessionAdmin(admin.ModelAdmin):
-    list_display = ("appointment", "room_name", "started_at", "ended_at")
--- app/back/health_platform/telemedicine/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import VideoSession
-
-class VideoSessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VideoSession
-        fields = ["id", "appointment", "room_name", "provider_token", "patient_token", "started_at", "ended_at"]
-        
